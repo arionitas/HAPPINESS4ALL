f148474ef9945def0da90e4152f3406ab984e731
arionitas <mgmt@arxivn.com>
2023-10-04 11:50:54 +0100

    push

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 function getNewQuote() {
    $.ajax({
-     url: 'https://api.forismatic.com/api/1.0/',
+     url: 'https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=xml&lang=en',
      data: {
         method: 'getQuote',
         lang: 'en',
