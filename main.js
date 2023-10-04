ca8ac9b40b07b117762a9fabbe2c712d80c50812
arionitas <mgmt@arxivn.com>
2023-10-04 11:17:08 +0100

    push

--- main.js
+++ main.js
@@ -1,19 +1,16 @@
 $(document).ready(function() {
-    function getNewQuote() {
-        $.ajax({
-            url: 'https://api.forismatic.com/api/1.0/',
-            data: {
-                method: 'getQuote',
-                lang: 'en',
-                format: 'json'
-            },
-            success: function(response)
-              console.console.log(response.quote); {
-                console.log(response.quoteTest)
-              }
-        });
-    }
-    getNewQuote();
+function getNewQuote() {
+   $.ajax({
+     url: 'https://api.forismatic.com/api/1.0/',
+     data: {
+        method: 'getQuote',
+        lang: 'en',
+        format: 'json'
+     },
+     success: function(response) {
+        console.log(response.quoteTest);
+     }
+   }); 
+  }
+  getNewQuote();
 });
-
-
